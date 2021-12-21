9dd883d5b969af812c0aa3209e8c8f61f80a23de
Yara Kassim <yskp2901@gmail.com>
2021-12-21 22:33:58 +0100

    rentree

--- MINI/main.c
+++ MINI/main.c
@@ -22,15 +22,38 @@ void    my_mlx_pixel_put(data_t *pix, int x, int y, int color)
     *(unsigned int*)dst = color;
 }
 
+void	draw_line(data_t *pix, int x, int y)
+{
+
+}
+
 int main(void)
 {
     data_t  data;
+    int x = 55;
+    int y = 55;
+    int i = 54;
 
     data.mlx_ptr = mlx_init();
     data.mlx_win = mlx_new_window(data.mlx_ptr, 1920, 1080, "fdf");
     data.img = mlx_new_image(data.mlx_ptr, 1920, 1080);
     data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
-    my_mlx_pixel_put(&data, 55, 55, 0x00FF0000);
+    while (++i < 500)
+    {
+	    my_mlx_pixel_put(&data, 55, y, 0x00FF0000);
+	    my_mlx_pixel_put(&data, x, 55, 0x00FF0000);
+	    x++;
+	    y++;
+    }
+    i = 54;
+    y = 55;
+    while (++i < 500)
+    { 
+	    my_mlx_pixel_put(&data, x, y, 0x00FF0000);
+	    x--;
+	    y++;
+    } 
+
     mlx_put_image_to_window(data.mlx_ptr, data.mlx_win, data.img, 0, 0);
     mlx_loop(data.mlx_ptr);
 }
